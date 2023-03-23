a4085f403e2a7c21e1792466f4350df5062d9221
Manos Koutoulakis <manos.koutoulak@gmail.com>
2023-03-23 13:15:08 +0200

    add the slice location into the exported metadata.json

--- utils/data_preperation.py
+++ utils/data_preperation.py
@@ -1,4 +1,6 @@
+from typing import Union
 import pydicom as dcm
+from pydicom import dicomdir
 import numpy as np
 import nibabel as nib
 import os
@@ -29,9 +31,8 @@ class Patient:
         self.patient_volume = self.get_volume()
         self.patient_masks = self.get_mask()
         self.output_path = output_path
-        # self.Preprocessing = Preprocessing(self.)
 
-    def get_patient_metadata(self,) -> dcm.dicomdir.DicomDir:
+    def get_patient_metadata(self,) -> Union[dcm.FileDataset,None]:
         """
         Each CT scan contains its own DIRFILE that contains 
         information for patient's metadata and the CT volume
@@ -42,31 +43,39 @@ class Patient:
             return None
         return metadata
 
-    def get_filenames(self,) -> list:
+    def get_filenames(self,) -> Union[list,None]:
         """
         This function is not completely right since we added hardcoded the second element
         TODO -> check a generalized method for getting the slice folder
         """
-        ct_folder = os.path.join(
-            self.ct_path, self.patient_metadata.DirectoryRecordSequence[0].ReferencedFileID[1])
-        return [os.path.join(ct_folder, i) for i in os.listdir(ct_folder)
+        try:
+            if self.patient_metadata:
+                ct_folder = os.path.join(
+                self.ct_path, self.patient_metadata.DirectoryRecordSequence[0].ReferencedFileID[1])
+                return [os.path.join(ct_folder, i) for i in os.listdir(ct_folder)
                 if i != "DIRFILE" and i != ".DS_Store"]
+        except ValueError as e:
+            raise ValueError("Failed to load patient metadata")
 
-    def dicom_extraction(self) -> np.ndarray:
+    def dicom_extraction(self, only_location=False) -> list:
         dicom_files = []
         for filename in self.get_filenames():
             sl = dcm.dcmread(filename)
+            # print(f"Image Position {sl.ImagePositionPatient}, Slice location: {sl.SliceLocation}")
             dicom_files.append(sl)
         # print(sl)
+        if only_location:
+            return sorted([float(x.SliceLocation) for x in dicom_files])
+
         return sorted(dicom_files, key=lambda x: float(x.SliceLocation))
 
-    def get_volume(self) -> np.array:
+    def get_volume(self) -> np.ndarray:
         return np.array([sl.pixel_array for sl in self.dicom_extraction()]).transpose(1, 2, 0).astype(np.float32)
 
     def get_modality(self) -> str:
         return self.patient_metadata.DirectoryRecordSequence[0].Modality
 
-    def get_mask(self) -> np.array:
+    def get_mask(self) -> np.ndarray:
         # print(f"{self.mask_path}, \n {self.ct_path}")
         mask = nib.load(self.mask_path).get_fdata()
         mask = np.swapaxes(mask, 1, 0).astype(np.uint8)
@@ -76,13 +85,13 @@ class Patient:
 
     def get_shape(self) -> tuple:
         return (
-            self.patient.DirectoryRecordSequence[0].Rows,
+            self.patient_metadata.DirectoryRecordSequence[0].Rows,
             self.patient_metadata.DirectoryRecordSequence[0].Columns,
             int(
                 self.patient_metadata.DirectoryRecordSequence[0].NumberOfSeriesRelatedInstances)
         )
 
-    def load_volume_parameters(self) -> None:
+    def load_volume_parameters(self) -> dict:
         sl = dcm.dcmread(self.get_filenames()[0])
         return {
             "SliceThickness": sl.SliceThickness,
@@ -93,22 +102,29 @@ class Patient:
         }
 
     def get_segmented_region(self):
-        masks = self.get_mask()
-        mask_size = masks.shape
+        mask_size = self.patient_masks.shape
+        sl_locations = self.dicom_extraction(only_location=True)
         indexes = []
+        locations = []
         # Loop through the slices of the mask image
-        for z in range(mask_size[2]):
-            slice = masks[:, :, z]
+        for z,loc in zip(range(mask_size[2]), sl_locations):
+            # print(self.patient_masks)
+            slice = self.patient_masks[:, :, z]
             if slice.any():
                 if len(np.nonzero(slice)[0]) < 100:
                     # If the pixel size of the segmented region
                     # is less than 100 the dont pass it
                     pass
                 indexes.append(z)
-        return (indexes)
+                locations.append(loc)
+        print(len(indexes),len(locations))
+        return {
+            "Indexes":indexes,
+            "SliceLocation":locations
+                }
 
     def data_ROI_only(self):
-        indexes = self.get_segmented_region()
+        indexes = self.get_segmented_region()["Indexes"]
         masks = []
         slices = []
         for i in indexes:
@@ -152,7 +168,7 @@ class Patient:
 
         return centroids
 
-    def get_bounding_box(self, cX, cY, box_size=(40, 40)):
+    def get_bounding_box(self, cX, cY, box_size=(40, 40)) -> tuple:
         """
         Function that returns the `top left` and `bottom right` corner of the bounding box
         """
@@ -164,7 +180,7 @@ class Patient:
 
         return (x1, y1, x2, y2)
 
-    def extract_json_file(self, ):
+    def extract_json_file(self, ) -> dict:
         patient_metadata = {}
         patient_metadata["metadata"] = self.load_volume_parameters(
         )
@@ -174,7 +190,7 @@ class Patient:
 
         return patient_metadata
 
-    def save_volume_with_ROI_only(self, save_metadata=True):
+    def save_volume_with_ROI_only(self, save_metadata=True) -> None:
         new_volume_path = os.path.join(self.output_path, "ct")
         new_mask_path = os.path.join(self.output_path, "mask")
         os.makedirs(new_volume_path, exist_ok=True)
