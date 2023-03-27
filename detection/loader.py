c69d1fc2483e687faaf9c7e7970fba41612788fa
Manos Koutoulakis <manos.koutoulak@gmail.com>
2023-03-27 16:20:31 +0300

    Modifications in dataloader (Not ready yet)

--- detection/loader.py
+++ detection/loader.py
@@ -7,24 +7,24 @@ import json
 import torch
 from typing import Tuple, Dict
 # Define the dataset
-from config import *
+from utils.config import *
 
 
 # Data structure
 # ouputs/images
-    # GM4A-109_000.npy
+# GM4A-109_000.npy
 # outputs/metadata.json
-    # [PatientID] (e.g "GM4A-109")
-        # ["centroids"]
-            # [Slice index] (e.g "0")
-                # ["bbox"]
-                    # List that contains multiple lists
-                    # (e.g
-                        # [
-                            # [273, 161, 313, 201],
-                            # [88, 159, 128, 199]
-                        # ] 
-                    # )
+# [PatientID] (e.g "GM4A-109")
+# ["centroids"]
+# [Slice index] (e.g "0")
+# ["bbox"]
+# List that contains multiple lists
+# (e.g
+# [
+# [273, 161, 313, 201],
+# [88, 159, 128, 199]
+# ]
+# )
 
 # "centroids": {
 #       "0": {
@@ -32,35 +32,33 @@ from config import *
 #           [293, 181],
 #           [108, 179]
 #         ],
-#         "bbox": 
+#         "bbox":
 #       },
 
 class BoneMarrowDataset(Dataset):
-    def __init__(self, data_path: str,ann_path:str,) -> None:
+    def __init__(self, data_path: str, ann_path: str,) -> None:
         self.data_path = data_path
         self.ann_path = ann_path
-        self.images_path = os.path.join(self.data_path,"images")
+        self.images_path = os.path.join(self.data_path, "images")
         self.ann = self.retrieve_metadata()
-        self.image_files = [os.path.join(self.images_path,filename) for filename in os.listdir(self.images_path) ]
-        
+        self.image_files = [os.path.join(
+            self.images_path, filename) for filename in os.listdir(self.images_path)]
 
     def retrieve_metadata(self) -> Dict:
         f = open(self.ann_path)
         return json.load(f)
-    
-    def fetch_img_info(self, filename : str)-> Tuple[str,int]:
+
+    def fetch_img_info(self, filename: str) -> Tuple[str, int]:
         patient_id = filename.split("/")[-1].split("_")[0]
         slice_num = int(filename.split("/")[-1].split("_")[-1].split(".")[0])
-        return patient_id,slice_num
+        return patient_id, slice_num
 
-    def __len__(self)-> int:
+    def __len__(self) -> int:
         return len(self.image_files)
 
-    def __getitem__(self, index) -> Tuple[torch.Tensor,torch.Tensor]:
+    def __getitem__(self, index) -> Tuple[torch.Tensor, torch.Tensor]:
         img_path = self.image_files[index]
-        patient_id, slice_num =  self.fetch_img_info(img_path)
+        patient_id, slice_num = self.fetch_img_info(img_path)
         image = np.load(img_path)
         targets = self.ann[patient_id]["centroids"][str(slice_num)]["bbox"]
-        return torch.tensor(image,dtype=torch.float32), torch.tensor(targets, dtype=torch.float32)
-
-
+        return torch.tensor(image, dtype=torch.float32), torch.tensor(targets, dtype=torch.float32)
