94c01f9c8c8e32424eb9655f94da3f607d59cfe2
manoskout <manos.koutoulak@gmail.com>
2023-04-01 12:50:51 +0300

    dataloader and pretrained FasterRCNN

--- detection/loader.py
+++ detection/loader.py
@@ -1,64 +1,101 @@
+from albumentations.pytorch import ToTensorV2
+from pycocotools.coco import COCO
 import torch.nn as nn
 import numpy as np
-from torch.utils.data import Dataset, DataLoader
-import torch.optim as optim
 import os
-import json
 import torch
-from typing import Tuple, Dict
 # Define the dataset
-from utils.config import *
+from torchvision import datasets
+# from utils.config import *
+import numpy as np  # linear algebra
+import os
+import torch
+from torch.nn import functional as F
+import copy
+from PIL import Image
+import albumentations as A
+
+import warnings
+warnings.filterwarnings("ignore")
 
 
-# Data structure
-# ouputs/images
-# GM4A-109_000.npy
-# outputs/metadata.json
-# [PatientID] (e.g "GM4A-109")
-# ["centroids"]
-# [Slice index] (e.g "0")
-# ["bbox"]
-# List that contains multiple lists
-# (e.g
-# [
-# [273, 161, 313, 201],
-# [88, 159, 128, 199]
-# ]
-# )
+def collate_fn(batch):
+    return tuple(zip(*batch))
 
-# "centroids": {
-#       "0": {
-#         "centroid": [
-#           [293, 181],
-#           [108, 179]
-#         ],
-#         "bbox":
-#       },
 
-class BoneMarrowDataset(Dataset):
-    def __init__(self, data_path: str, ann_path: str,) -> None:
-        self.data_path = data_path
-        self.ann_path = ann_path
-        self.images_path = os.path.join(self.data_path, "images")
-        self.ann = self.retrieve_metadata()
-        self.image_files = [os.path.join(
-            self.images_path, filename) for filename in os.listdir(self.images_path)]
+def get_albumentation(train):
+    if train:
+        transform = A.Compose([
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            ToTensorV2()
+        ], bbox_params=A.BboxParams(format='coco'))
+    else:
+        transform = A.Compose([
+            ToTensorV2()
+        ], bbox_params=A.BboxParams(format='coco'))
+    return transform
 
-    def retrieve_metadata(self) -> Dict:
-        f = open(self.ann_path)
-        return json.load(f)
 
-    def fetch_img_info(self, filename: str) -> Tuple[str, int]:
-        patient_id = filename.split("/")[-1].split("_")[0]
-        slice_num = int(filename.split("/")[-1].split("_")[-1].split(".")[0])
-        return patient_id, slice_num
+class CTDetectionDataset(datasets.VisionDataset):
+    def __init__(
+        self,
+        root: str,
+        split="train",
+        transform=None,
+        target_transform=None,
+        transforms=None,
+    ) -> None:
+        super().__init__(root, transforms, transform, target_transform)
+        self.split = split
+        self.coco = COCO(os.path.join(
+            root, split+"/images", "_annotations.coco.json"))
+        self.ids = list(sorted(self.coco.imgs.keys()))
+        print(self.ids)
+        self.target_ids = [id for id in self.ids if (
+            len(self._load_target(id)) > 0)]
+
+    def _load_image(self, id: int) -> Image.Image:
+        path = self.coco.loadImgs(id)[0]["file_name"]
+        image = np.load(os.path.join(self.root, self.split+"/images", path))
+        return image
+
+    def _load_target(self, id: int):
+        return self.coco.loadAnns(self.coco.getAnnIds(id))
+
+    def __getitem__(self, index: int):
+        id = self.ids[index]
+        image = self._load_image(id)
+        target = copy.deepcopy(self._load_target(id))
+
+        boxes = [t['bbox'] + [t['category_id']] for t in target]
+        if self.transforms is not None:
+            transformed = self.transforms(image=image, bboxes=boxes)
+            # print(transformed)
+            image = transformed['image']
+            boxes = transformed['bboxes']
+        new_boxes = []
+        for box in boxes:
+            xmin = box[0]
+            ymin = box[1]
+            xmax = xmin + box[2]
+            ymax = ymin + box[3]
+            new_boxes.append([xmin, ymin, xmax, ymax])
+
+        boxes = torch.tensor(new_boxes, dtype=torch.float32)
+
+        targ = {}
+        targ["boxes"] = boxes
+        targ["labels"] = torch.tensor(
+            [t["category_id"] for t in target], dtype=torch.int64)
+        targ["image_id"] = torch.tensor([t["image_id"] for t in target])
+        targ["area"] = (boxes[:, 3] - boxes[:, 1]) * \
+            (boxes[:, 2] - boxes[:, 0])
+        # Not defined .... Manually setted to 0
+        # targ["iscrowd"] = torch.tensor([t["iscrowd"]  for t in target], dtype=torch.int64)
+        targ["iscrowd"] = torch.tensor([1 for t in target], dtype=torch.int64)
+
+        return image, targ
 
     def __len__(self) -> int:
-        return len(self.image_files)
-
-    def __getitem__(self, index) -> Tuple[torch.Tensor, torch.Tensor]:
-        img_path = self.image_files[index]
-        patient_id, slice_num = self.fetch_img_info(img_path)
-        image = np.load(img_path)
-        targets = self.ann[patient_id]["centroids"][str(slice_num)]["bbox"]
-        return torch.tensor(image, dtype=torch.float32), torch.tensor(targets, dtype=torch.float32)
+        return len(self.ids)
