6b6ca2716b77cd7d3041fd8b05064debe788921e
manoskout <manos.koutoulak@gmail.com>
2023-04-01 12:28:35 +0300

    edit conf file

--- utils/config.py
+++ utils/config.py
@@ -2,9 +2,16 @@ import os
 
 DICOM_PATH = "../data/ct/"
 ROI_PATH = "../data/roi/"
+
 OUTPUT_PATH = "../data/output"
 TRAINING_PATH = "../data/output/train"
 TESTING_PATH = "../data/output/test"
+VALIDATION_PATH = "../data/output/valid"
+
+
+INPUT_SIZE = (600, 600)
+
+WINDOWING = "soft_tissue"
 
 PATIENTS = [i for i in os.listdir(
     DICOM_PATH) if i != "DIRFILE" and i != ".DS_Store"]
